d33d64492957f8a17b3ebb2d24d48eb26a5724fa
glawkdxn0328 <glawkdxn0328@naver.com>
2022-10-09 16:24:34 +0900

    hashtable

--- hashtable/src/hashTable.java
+++ hashtable/src/hashTable.java
@@ -15,7 +15,8 @@ public class hashTable {
             return h;
         }
         //해당 위치에 이미 데이터가 있다면?->open address vs seperate chaining
-        else {
+        else{
+            open_address(h);
 
         }
 
@@ -34,8 +35,14 @@ public class hashTable {
     }
 
 //hash collusion : 같은 주소가 나왔을 때 처리 방법.
-    public void open_address(){
-        //그뒤에 붙이기
+    public int open_address(int h){
+        //h에 +1하는 방식으로 진행할 경우, h가 테이블의 끝이고 이미 값이 있을 경우 앞부분 index가 비어있어도 메모리 할당이 불가능해진다.
+        for(int i=1; i<getCapacity();i++){
+            if (table[h+i]!=null){
+                return (h+i);
+            }
+        }
+        return 0;
     }
 
     public void seperate_chaining(){
